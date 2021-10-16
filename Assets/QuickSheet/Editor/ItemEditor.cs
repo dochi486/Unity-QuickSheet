dcfecdf594b7cc7da02105ce6b5eeb4bca1a4a57
dochi486 <93mskim@gmail.com>
2021-10-17 00:49:09 +0900

    commit test

--- Assets/QuickSheet/Editor/ItemEditor.cs
+++ Assets/QuickSheet/Editor/ItemEditor.cs
@@ -4,42 +4,42 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
-using GDataDB;
-using GDataDB.Linq;
+//using GDataDB;
+//using GDataDB.Linq;
 
 using UnityQuickSheet;
 
 ///
 /// !!! Machine generated code !!!
 ///
-[CustomEditor(typeof(Item))]
-public class ItemEditor : BaseGoogleEditor<Item>
-{	    
-    public override bool Load()
-    {        
-        Item targetData = target as Item;
+//[CustomEditor(typeof(Item))]
+//public class ItemEditor : BaseGoogleEditor<Item>
+//{	    
+//    public override bool Load()
+//    {        
+//        Item targetData = target as Item;
         
-        var client = new DatabaseClient("", "");
-        string error = string.Empty;
-        var db = client.GetDatabase(targetData.SheetName, ref error);	
-        var table = db.GetTable<ItemData>(targetData.WorksheetName) ?? db.CreateTable<ItemData>(targetData.WorksheetName);
+//        var client = new DatabaseClient("", "");
+//        string error = string.Empty;
+//        var db = client.GetDatabase(targetData.SheetName, ref error);	
+//        var table = db.GetTable<ItemData>(targetData.WorksheetName) ?? db.CreateTable<ItemData>(targetData.WorksheetName);
         
-        List<ItemData> myDataList = new List<ItemData>();
+//        List<ItemData> myDataList = new List<ItemData>();
         
-        var all = table.FindAll();
-        foreach(var elem in all)
-        {
-            ItemData data = new ItemData();
+//        var all = table.FindAll();
+//        foreach(var elem in all)
+//        {
+//            ItemData data = new ItemData();
             
-            data = Cloner.DeepCopy<ItemData>(elem.Element);
-            myDataList.Add(data);
-        }
+//            data = Cloner.DeepCopy<ItemData>(elem.Element);
+//            myDataList.Add(data);
+//        }
                 
-        targetData.dataArray = myDataList.ToArray();
+//        targetData.dataArray = myDataList.ToArray();
         
-        EditorUtility.SetDirty(targetData);
-        AssetDatabase.SaveAssets();
+//        EditorUtility.SetDirty(targetData);
+//        AssetDatabase.SaveAssets();
         
-        return true;
-    }
-}
+//        return true;
+//    }
+//}
